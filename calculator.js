2c51e3f39b9130b6d8d058a99272adce485bbcc1
Amandine Perichon <apericho@gmail.com>
2016-07-05 15:02:13 +1200

    Add fix for two operators in a row

--- calculator.js
+++ calculator.js
@@ -1,17 +1,27 @@
 $(document).ready(function(){
-  var input = "";
+  var input = ""
+  var display = ""
   $(".num").click(function(evt){
    input = input + ($(evt.target).html())
-   $(".display").text(input)
+   display = display + ($(evt.target).html())
+   $(".display").text(display)
   }) 
   $(".operator").click(function(evt){
-    var operator 
-    operator = ($(evt.target).html()) 
+    var operator = ($(evt.target).html()) 
+    var length = input.length
+    if (input[input.length-1] === "+" | 
+        input[input.length-1] === "-" |
+        input[input.length-1] === "/" | 
+        input[input.length-1] === "X") {
+      input = input.substring(0, input.length-1)
+    }
     if (operator === "X"){
       operator = "*"
     } 
-    input = input + operator
+    input = eval(input)
     $(".display").text(input)
+    input = input + operator
+    display = ""
   })
   $(".equal").click(function(){
     $(".display").text(eval(input))
@@ -22,13 +32,14 @@ $(document).ready(function(){
   })
   $(".ac").click(function(){
     input = ""
+    display = ""
     $(".display").text(input)
   })
   $(".ce").click(function(){
     var splitArr = input.split(/[\+\-\*\/]/)
     var length = splitArr.length
     var last = splitArr[length-1]
-    input = input.substring(0,input.length-last.length)
+    input = input.substring(0,input.length-last.length-1)
     $(".display").text(input)
   })
 })
